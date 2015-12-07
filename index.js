3496f13674808bade2c12b74cfe18d61e3c05bd4
Jimb Esser <wasteland@gmail.com>
2015-12-07 09:46:52 -0800

    Bump version and fix typo.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ try {
   var binding = require('./build/Release/mtrace.node');
   module.exports = binding;
 } catch (e) {
-  console.error('error loading mtracer: ' + e.stack);
+  console.error('error loading mtrace: ' + e.stack);
   module.exports = {
     mtrace: function() { },
     muntrace: function() { },
